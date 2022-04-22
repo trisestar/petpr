18be9fcd0b29bce6c4b370c8a9f32c5d2229d023
Artyom <a21plusartyom@gmail.com>
2022-04-22 08:41:51 +0300

    implemented config server

--- webclient/src/main/java/com/petpr/webclient/controller/Controller.java
+++ webclient/src/main/java/com/petpr/webclient/controller/Controller.java
@@ -2,6 +2,8 @@ package com.petpr.webclient.controller;
 
 import com.petpr.webclient.Webclient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +14,19 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class Controller {
 
     @Autowired
     private Webclient webclient;
 
+    @Value("${datasource-driver-class-name}")
+    private String propertyObj;
+
     @GetMapping("/")
     public Mono get() {
         System.out.println("webclient called get " + LocalDateTime.now());
-        return Mono.just("123");
+        return Mono.just(propertyObj);
     }
 
     @PostMapping("/webclient")
